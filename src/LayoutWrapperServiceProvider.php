eeef94ccb742e24079e41e17b2662f1f619a7a84
thejmitchener <joshua@fuelviews.com>
2024-06-12 16:51:10 -0400

    Add bug report issue template, config file, dependabot configuration, funding file, auto-release workflow, dependabot auto-merge workflow, fix PHP code style issues workflow, open AI PR description workflow, run tests workflow, and update changelog workflow. The changes were made to improve the development process, enhance code quality, and streamline project management.

--- src/LayoutWrapperServiceProvider.php
+++ src/LayoutWrapperServiceProvider.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\File;
 
 class LayoutWrapperServiceProvider extends PackageServiceProvider
 {
-    const MARKER_FILE = 'layout_wrapper_installed';
-
     public function configurePackage(Package $package): void
     {
         $package
@@ -18,23 +16,5 @@ class LayoutWrapperServiceProvider extends PackageServiceProvider
             ->hasConfigFile('layout-wrapper')
             ->hasViews()
             ->hasCommand(LayoutWrapperInstallCommand::class);
-    }
-
-    public function bootingPackage()
-    {
-        $this->autoPublishStubs();
-    }
-
-    protected function autoPublishStubs(): void
-    {
-        $markerPath = storage_path(self::MARKER_FILE);
-
-        if (!File::exists($markerPath)) {
-
-            File::copy(__DIR__ . '../../stubs/welcome.blade.php', resource_path('views/welcome.blade.php'));
-
-            File::put($markerPath, 'installed');
-
-        }
     }
 }
