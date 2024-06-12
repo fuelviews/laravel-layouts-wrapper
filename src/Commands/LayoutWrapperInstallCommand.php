e4308a18675697bb603745415dbf8505aeaf0bb5
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2024-06-12 17:07:49 -0400

    Refactored Livewire conditional statements and added a new command for installing the layout wrapper package. (#1)
    * Refactored Livewire conditional statements and added a new command for installing the layout wrapper package.
    
    * Add bug report issue template, config file, dependabot configuration, funding file, auto-release workflow, dependabot auto-merge workflow, fix PHP code style issues workflow, open AI PR description workflow, run tests workflow, and update changelog workflow. The changes were made to improve the development process, enhance code quality, and streamline project management.
    
    * Fix styling
    
    ---------
    
    Co-authored-by: thejmitchener <thejmitchener@users.noreply.github.com>

---
+++ src/Commands/LayoutWrapperInstallCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fuelviews\LayoutWrapper\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class LayoutWrapperInstallCommand extends Command
+{
+    public $signature = 'layout-wrapper:install';
+
+    public $description = 'Install the layout wrapper package';
+
+    public function handle(): int
+    {
+        File::copy(__DIR__.'/../../stubs/welcome.blade.php', resource_path('views/welcome.blade.php'));
+
+        $this->info('welcome.blade.php has been published successfully.');
+
+        return self::SUCCESS;
+    }
+}
--- src/LayoutWrapperServiceProvider.php
+++ src/LayoutWrapperServiceProvider.php
@@ -2,39 +2,18 @@
 
 namespace Fuelviews\LayoutWrapper;
 
-use Fuelviews\LayoutWrapper\Commands\LayoutWrapperCommand;
+use Fuelviews\LayoutWrapper\Commands\LayoutWrapperInstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Illuminate\Support\Facades\File;
 
 class LayoutWrapperServiceProvider extends PackageServiceProvider
 {
-    const MARKER_FILE = 'layout_wrapper_installed';
-
     public function configurePackage(Package $package): void
     {
         $package
             ->name('layout-wrapper')
             ->hasConfigFile('layout-wrapper')
             ->hasViews()
-            ->hasCommand(LayoutWrapperCommand::class);
-    }
-
-    public function bootingPackage()
-    {
-        $this->autoPublishStubs();
-    }
-
-    protected function autoPublishStubs(): void
-    {
-        $markerPath = storage_path(self::MARKER_FILE);
-
-        if (!File::exists($markerPath)) {
-
-            File::copy(__DIR__ . '../../stubs/welcome.blade.php', resource_path('views/welcome.blade.php'));
-
-            File::put($markerPath, 'installed');
-
-        }
+            ->hasCommand(LayoutWrapperInstallCommand::class);
     }
 }
