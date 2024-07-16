e1d4da6a9e971fc75d3921df08d1750ff42ab9e8
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2024-07-16 13:34:22 -0400

    Refactor app.blade.php layout file to improve SEO and add support for Google Fonts and Google Tag Manager. (#11)

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,41 +1,42 @@
 <!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ app()->getLocale() }}">
+<head>
+    @if (class_exists(RalphJSmit\Laravel\SEO\SEOManager::class))
+        {!! seo() !!}
+    @endif
 
-        @if (View::exists('googletagmanager::head') && config('layouts-wrapper.gtm_enabled'))
-            @include('googletagmanager::head')
-        @endif
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @if (class_exists(\Livewire\Livewire::class) && config('layouts-wrapper.livewire_enabled'))
+        @livewireStyles
+    @endif
 
-        @if (class_exists(\Livewire\Livewire::class) && config('layouts-wrapper.livewire_enabled'))
-            @livewireStyles
-        @endif
+    @if(class_exists(\Spatie\GoogleFonts\GoogleFonts::class))
+        @googlefonts
+    @endif
 
-        @if(class_exists(\Spatie\GoogleFonts\GoogleFonts::class))
-            @googlefonts
-        @endif
-    </head>
-    <body>
-        @if (View::exists('navigation::components.navigation') && config('layouts-wrapper.navigation_enabled'))
-            <x-navigation::navigation />
-        @endif
+    @if (View::exists('googletagmanager::head') && config('layouts-wrapper.gtm_enabled'))
+        @include('googletagmanager::head')
+    @endif
+</head>
+<body>
+    @if (View::exists('navigation::components.navigation') && config('layouts-wrapper.navigation_enabled'))
+        <x-navigation::navigation />
+    @endif
 
-        {{ $slot }}
+    {{ $slot }}
 
-        @if (View::exists('forms::livewire.forms-modal') && config('layouts-wrapper.forms_modal_enabled'))
-            @livewire('forms-modal')
-        @endif
+    @if (View::exists('forms::livewire.forms-modal') && config('layouts-wrapper.forms_modal_enabled'))
+        @livewire('forms-modal')
+    @endif
 
-        @if (View::exists('googletagmanager::body') && config('layouts-wrapper.gtm_enabled'))
-            @include('googletagmanager::body')
-        @endif
+    @if (View::exists('googletagmanager::body') && config('layouts-wrapper.gtm_enabled'))
+        @include('googletagmanager::body')
+    @endif
 
-        @if (class_exists(\Livewire\Livewire::class) && config('layouts-wrapper.livewire_enabled'))
-            @livewireScripts
-        @endif
-    </body>
+    @if (class_exists(\Livewire\Livewire::class) && config('layouts-wrapper.livewire_enabled'))
+        @livewireScripts
+    @endif
+</body>
 </html>
 
