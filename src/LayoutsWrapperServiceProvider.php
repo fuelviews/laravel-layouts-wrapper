0e343371ac49f71fc981bbead4161aa92e01375f
Joshua Mitchener <64335706+thejmitchener@users.noreply.github.com>
2024-07-27 14:48:00 -0400

    Refactor stubs add seo (#13)
    * Add ralphjsmit/laravel-seo package to composer.json and create migration and seeder files for the pages table. Also, create a new model Page with SEO functionality.
    
    * Fix styling
    
    ---------
    
    Co-authored-by: thejmitchener <thejmitchener@users.noreply.github.com>

--- src/LayoutsWrapperServiceProvider.php
+++ src/LayoutsWrapperServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Fuelviews\LayoutsWrapper;
 
-use Fuelviews\LayoutsWrapper\Commands\LayoutsWrapperInstallCommand;
+use App\Models\Page;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -14,6 +17,36 @@ class LayoutsWrapperServiceProvider extends PackageServiceProvider
             ->name('layouts-wrapper')
             ->hasConfigFile('layouts-wrapper')
             ->hasViews()
-            ->hasCommand(LayoutsWrapperInstallCommand::class);
+            ->hasMigration('create_pages_table')
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations();
+            });
+
+        $this->publishes([
+            __DIR__.'/../database/seeders/PagesTableSeeder.php.stub' => database_path('seeders/PagesTableSeeder.php'),
+        ], 'layouts-wrapper-seeders');
+
+        $this->publishes([
+            __DIR__.'/Models/Page.php' => app_path('Models/Page.php'),
+        ], 'layouts-wrapper-models');
+
+        $this->publishes([
+            __DIR__.'/../resources/stubs/welcome.blade.php.stub' => resource_path('views/welcome.blade.php'),
+        ], 'layouts-wrapper-welcome');
+    }
+
+    public function bootingPackage(): void
+    {
+        View::composer('layouts-wrapper::components.layouts.app', function ($view) {
+            $routeName = Route::currentRouteName();
+
+            $page = Page::where('slug', $routeName)
+                ->first();
+
+            $view->with('page', $page);
+        });
     }
 }
